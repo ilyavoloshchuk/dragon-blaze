141e1a34c489a847e0826f87c540b8ea8be37e90
ilya <ilya.voloshchuk@nikcode.net>
2024-11-22 23:46:48 +0200

    [*] Parallax

--- Assets/Scripts/Parallax/ParallaxBackground.cs
+++ Assets/Scripts/Parallax/ParallaxBackground.cs
@@ -1,99 +1,93 @@
 using UnityEngine;
 
-public class ParallaxBackground : MonoBehaviour
+namespace Parallax
 {
-    #region Serialized Fields
-    [SerializeField] private Vector2 parallaxEffectMultiplier;
-    [SerializeField] private bool infiniteHorizontal;
-    [SerializeField] private bool infiniteVertical;
-    #endregion
-
-    #region Private Fields
-    private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
-    private float textureUnitSizeY;
-    private SpriteRenderer spriteRenderer;
-    #endregion
-
-    #region Unity Lifecycle Methods
-    void Start()
+    public class ParallaxBackground : MonoBehaviour
     {
-        InitializeComponents();
-        SetupTextureSize();
-    }
+        [SerializeField] private Vector2 parallaxEffectMultiplier;
+        [SerializeField] private bool infiniteHorizontal;
+        [SerializeField] private bool infiniteVertical;
+        
+        private Transform cameraTransform;
+        private Vector3 lastCameraPosition;
+        private float textureUnitSizeX;
+        private float textureUnitSizeY;
+        private SpriteRenderer spriteRenderer;
 
-    private void LateUpdate()
-    {
-        if (cameraTransform == null) return;
-
-        ApplyParallaxEffect();
-        HandleInfiniteScrolling();
-    }
-    #endregion
-
-    #region Private Methods
-    private void InitializeComponents()
-    {
-        cameraTransform = Camera.main?.transform;
-        if (cameraTransform == null)
+        private void Start()
         {
-            Debug.LogError("Camera.main is not found. ParallaxBackground requires a main camera.");
-            return;
+            InitializeComponents();
+            SetupTextureSize();
         }
 
-        lastCameraPosition = cameraTransform.position;
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer == null)
+        private void LateUpdate()
         {
-            Debug.LogError("SpriteRenderer component not found on the object.");
-            return;
+            if (cameraTransform == null) return;
+
+            ApplyParallaxEffect();
+            HandleInfiniteScrolling();
         }
-    }
-
-    private void SetupTextureSize()
-    {
-        Texture2D texture = spriteRenderer.sprite.texture;
-        textureUnitSizeX = texture.width / spriteRenderer.sprite.pixelsPerUnit;
-        textureUnitSizeY = texture.height / spriteRenderer.sprite.pixelsPerUnit;
-    }
-
-    private void ApplyParallaxEffect()
-    {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
-        lastCameraPosition = cameraTransform.position;
-    }
-
-    private void HandleInfiniteScrolling()
-    {
-        if (infiniteHorizontal)
+        
+        private void InitializeComponents()
         {
-            AdjustHorizontalPosition();
+            cameraTransform = Camera.main?.transform;
+            if (cameraTransform == null)
+            {
+                Debug.LogError("Camera.main is not found. ParallaxBackground requires a main camera.");
+                return;
+            }
+
+            lastCameraPosition = cameraTransform.position;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null)
+            {
+                Debug.LogError("SpriteRenderer component not found on the object.");
+            }
         }
 
-        if (infiniteVertical)
+        private void SetupTextureSize()
         {
-            AdjustVerticalPosition();
+            Texture2D texture = spriteRenderer.sprite.texture;
+            textureUnitSizeX = texture.width / spriteRenderer.sprite.pixelsPerUnit;
+            textureUnitSizeY = texture.height / spriteRenderer.sprite.pixelsPerUnit;
         }
-    }
 
-    private void AdjustHorizontalPosition()
-    {
-        float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-        if (Mathf.Abs(offsetPositionX) >= textureUnitSizeX / 2)
+        private void ApplyParallaxEffect()
         {
-            transform.position = new Vector3(cameraTransform.position.x - offsetPositionX, transform.position.y);
+            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+            transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+            lastCameraPosition = cameraTransform.position;
         }
-    }
 
-    private void AdjustVerticalPosition()
-    {
-        float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
-        if (Mathf.Abs(offsetPositionY) >= textureUnitSizeY / 2)
+        private void HandleInfiniteScrolling()
         {
-            transform.position = new Vector3(transform.position.x, cameraTransform.position.y - offsetPositionY);
+            if (infiniteHorizontal)
+            {
+                AdjustHorizontalPosition();
+            }
+
+            if (infiniteVertical)
+            {
+                AdjustVerticalPosition();
+            }
+        }
+
+        private void AdjustHorizontalPosition()
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            if (Mathf.Abs(offsetPositionX) >= textureUnitSizeX / 2)
+            {
+                transform.position = new Vector3(cameraTransform.position.x - offsetPositionX, transform.position.y);
+            }
+        }
+
+        private void AdjustVerticalPosition()
+        {
+            float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+            if (Mathf.Abs(offsetPositionY) >= textureUnitSizeY / 2)
+            {
+                transform.position = new Vector3(transform.position.x, cameraTransform.position.y - offsetPositionY);
+            }
         }
     }
-    #endregion
 }
