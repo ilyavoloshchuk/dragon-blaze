800fbefb6267004cfc37b5e3f1e33146eae4df67
ilya <ilya.voloshchuk@nikcode.net>
2024-11-22 23:36:32 +0200

    [*] Npc

--- Assets/Scripts/NPCs/Dialogue/DialogueController.cs
+++ Assets/Scripts/NPCs/Dialogue/DialogueController.cs
@@ -1,131 +1,124 @@
-using UnityEngine;
-using TMPro;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using Core;
+using TMPro;
+using UnityEngine;
 
-public class DialogueController : MonoBehaviour
+namespace NPCs.Dialogue
 {
-    #region Serialized Fields
-    [SerializeField] private TextMeshProUGUI NPCNameText;
-    [SerializeField] private TextMeshProUGUI NPCDialogueText;
-    [SerializeField] private float typeSpeed = 10f;
-    [SerializeField] private AudioClip dialogueSound;
-    #endregion
-
-    #region Public Fields
-    public PlayerMovement playerMovement;
-    #endregion
-
-    #region Private Fields
-    private Queue<string> paragraphs = new Queue<string>();
-    private bool conversationEnded;
-    private string p;
-    private Coroutine typeDialogueCoroutine;
-    private bool isTyping;
-    private const float MAX_TYPE_TIME = 0.1f;
-    #endregion
-
-    #region Public Methods
-    public void DisplayNextParagraph(DialogueText dialogueText, AudioClip dialogueSound = null)
+    public class DialogueController : MonoBehaviour
     {
-        if (paragraphs.Count == 0)
-        {
-            if (!conversationEnded)
-            {
-                StartConversation(dialogueText, dialogueSound);
-            }
-            else if (conversationEnded && !isTyping)
-            {
-                EndConversation();
-                return;
-            }
-        }
-
-        if (!isTyping)
-        {
-            p = paragraphs.Dequeue();
-            typeDialogueCoroutine = StartCoroutine(TypeDialogueText(p));
-        }
-        else
-        {
-            FinishParagraphEarly();
-        }
-
-        if (paragraphs.Count == 0)
-        {
-            conversationEnded = true;
-        }
-    }
-    #endregion
-
-    #region Private Methods
-    private void StartConversation(DialogueText dialogueText, AudioClip dialogueSound = null)
-    {
-        playerMovement.setInteracting(true);
-        playerMovement.enabled = false;
-
-        if (dialogueSound != null)
-        {
-            SoundManager.instance.PlaySound(dialogueSound);
-        }
-
-        if (!gameObject.activeSelf)
-        {
-            gameObject.SetActive(true);
-        }
-        Time.timeScale = 1f;
-
-        NPCNameText.text = dialogueText.speakerName;
-
-        foreach (string paragraph in dialogueText.paragraphs)
-        {
-            paragraphs.Enqueue(paragraph);
-        }
-    }
-
-    private void EndConversation()
-    {
-        playerMovement.setInteracting(false);
-        playerMovement.enabled = true;
-
-        paragraphs.Clear();
-        conversationEnded = false;
-
-        if (gameObject.activeSelf)
-        {
-            gameObject.SetActive(false);
-        }
-        Time.timeScale = 1f;
-    }
-
-    private IEnumerator TypeDialogueText(string p)
-    {
-        isTyping = true;
-        int maxVisibleChars = 0;
-
-        NPCDialogueText.text = p;
-        NPCDialogueText.maxVisibleCharacters = maxVisibleChars;        
-
-        foreach (char c in p.ToCharArray())
-        {
-            maxVisibleChars++;
-            NPCDialogueText.maxVisibleCharacters = maxVisibleChars;
-
-            yield return new WaitForSeconds(MAX_TYPE_TIME / typeSpeed);
-        }
-
-        isTyping = false;
-    }
-
-    private void FinishParagraphEarly()
-    {
-        StopCoroutine(typeDialogueCoroutine);
-
-        NPCDialogueText.maxVisibleCharacters = p.Length;
-        NPCDialogueText.text = p;
+        [SerializeField] private TextMeshProUGUI NPCNameText;
+        [SerializeField] private TextMeshProUGUI NPCDialogueText;
+        [SerializeField] private float typeSpeed = 10f;
+        [SerializeField] private AudioClip dialogueSound;
         
-        isTyping = false;
+        public PlayerMovement playerMovement;
+        
+        private readonly Queue<string> paragraphs = new();
+        private bool conversationEnded;
+        private string p;
+        private Coroutine typeDialogueCoroutine;
+        private bool isTyping;
+        private const float MaxTypeTime = 0.1f;
+        
+        public void DisplayNextParagraph(DialogueText dialogueText, AudioClip dialogueSound = null)
+        {
+            if (paragraphs.Count == 0)
+            {
+                if (!conversationEnded)
+                {
+                    StartConversation(dialogueText, dialogueSound);
+                }
+                else if (conversationEnded && !isTyping)
+                {
+                    EndConversation();
+                    return;
+                }
+            }
+
+            if (!isTyping)
+            {
+                p = paragraphs.Dequeue();
+                typeDialogueCoroutine = StartCoroutine(TypeDialogueText(p));
+            }
+            else
+            {
+                FinishParagraphEarly();
+            }
+
+            if (paragraphs.Count == 0)
+            {
+                conversationEnded = true;
+            }
+        }
+        
+        private void StartConversation(DialogueText dialogueText, AudioClip dialogueSound = null)
+        {
+            playerMovement.setInteracting(true);
+            playerMovement.enabled = false;
+
+            if (dialogueSound != null)
+            {
+                SoundManager.instance.PlaySound(dialogueSound);
+            }
+
+            if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true);
+            }
+            Time.timeScale = 1f;
+
+            NPCNameText.text = dialogueText.speakerName;
+
+            foreach (string paragraph in dialogueText.paragraphs)
+            {
+                paragraphs.Enqueue(paragraph);
+            }
+        }
+
+        private void EndConversation()
+        {
+            playerMovement.setInteracting(false);
+            playerMovement.enabled = true;
+
+            paragraphs.Clear();
+            conversationEnded = false;
+
+            if (gameObject.activeSelf)
+            {
+                gameObject.SetActive(false);
+            }
+            Time.timeScale = 1f;
+        }
+
+        private IEnumerator TypeDialogueText(string p)
+        {
+            isTyping = true;
+            int maxVisibleChars = 0;
+
+            NPCDialogueText.text = p;
+            NPCDialogueText.maxVisibleCharacters = maxVisibleChars;        
+
+            foreach (char c in p.ToCharArray())
+            {
+                maxVisibleChars++;
+                NPCDialogueText.maxVisibleCharacters = maxVisibleChars;
+
+                yield return new WaitForSeconds(MaxTypeTime / typeSpeed);
+            }
+
+            isTyping = false;
+        }
+
+        private void FinishParagraphEarly()
+        {
+            StopCoroutine(typeDialogueCoroutine);
+
+            NPCDialogueText.maxVisibleCharacters = p.Length;
+            NPCDialogueText.text = p;
+        
+            isTyping = false;
+        }
     }
-    #endregion
 }
--- Assets/Scripts/NPCs/Npc's/Wraith.cs
+++ Assets/Scripts/NPCs/Npc's/Wraith.cs
@@ -1,22 +1,22 @@
+using NPCs.Dialogue;
 using UnityEngine;
 
-public class Wraith : NPC, ITalkable
+namespace NPCs.Npc_s
 {
-    #region Serialized Fields
-    [SerializeField] private DialogueText dialogueText;
-    [SerializeField] private DialogueController dialogueController;
-    [SerializeField] private AudioClip dialogueSound;
-    #endregion
-
-    #region Public Methods
-    public override void Interact()
+    public class Wraith : Npc
     {
-        Talk(dialogueText);
-    }
+        [SerializeField] private DialogueText dialogueText;
+        [SerializeField] private DialogueController dialogueController;
+        [SerializeField] private AudioClip dialogueSound;
 
-    public void Talk(DialogueText dialogueText)
-    {
-        dialogueController.DisplayNextParagraph(dialogueText, dialogueSound);
+        protected override void Interact()
+        {
+            Talk(dialogueText);
+        }
+
+        private void Talk(DialogueText dialogueText)
+        {
+            dialogueController.DisplayNextParagraph(dialogueText, dialogueSound);
+        }
     }
-    #endregion
 }
